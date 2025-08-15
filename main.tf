cac7a2897acc8b004a0f94276757ca90394be2d0
Oxdami <dami.devlabs@gmail.com>
2025-08-15 07:31:26 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "dami-test-bucket"
+
+  tags = {
+    Name        = "dami-test-bucket"
+    Environment = "Dev"
+  }
+}
